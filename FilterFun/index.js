ee18d1c58c7d522cdd3346b4329b47f53de1bfc5
abhishekkumarraj966 <abhishekkumarraj62073@gmail.com>
2024-09-13 23:43:50 +0530

    sdfghjk

--- FilterFun/index.js
+++
@@ -1,7 +0,0 @@
-  
-const arr=[1,6,2,3,4,5,6,7,8,9,6];
-const result=arr.filter((item)=>{
-//  return item<=6
-    result===6
-})
-console.table(result)
--- Path/index.js
+++
@@ -1,9 +0,0 @@
- const path=require("path")
-
- console.log(path.dirname("D:/Node Js/Path/path.js"));
- console.log(path.extname("D:/Node Js/Path/path.js"));
- console.log(path.basename("D:/Node Js/Path/path.js"));
-
- console.log(path.parse("D:/Node Js/Path/path.js"));
-  const mypath=path.parse("D:/Node Js/Path/path.js");
-   console.log(mypath.name);
